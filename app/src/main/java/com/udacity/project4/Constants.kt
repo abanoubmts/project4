f7bedd4e781fe5bcbd8e5a61c4113a8c10f7c711
abanoub.samirfahmi@gmail.com <abanoub.samirfahmi@gmail.com>
2022-10-10 11:56:04 +0200

    rebuild number 2 for project 4

---
+++ app/src/main/java/com/udacity/project4/Constants.kt
@@ -0,0 +1,13 @@
+package com.udacity.project4
+
+import com.google.android.gms.location.Geofence
+
+object Constants {
+    const val ACTION_GEOFENCE_EVENT = "SaveReminder.reminder.action.ACTION_GEOFENCE_EVENT"
+    const val GEOFENCE_RADIUS_IN_METERS = 100f
+    const val EXTRA_GEOFENCE_INDEX = "GEOFENCE_INDEX"
+    const val NEVER_EXPIRES = Geofence.NEVER_EXPIRE
+     const val JOB_ID = 573
+     const val TAG = "GeofenceIntentSer"
+
+}
